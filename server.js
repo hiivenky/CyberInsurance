762b0933300c8bbaad5f41862ff5091ba974bf28
Lenovo <mullagirivenkatesh23@gmail.com>
2020-08-16 15:59:10 +0530

    not displaying home page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname+'/dist/cyber-insurance'));
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/cyber-insurance/index.html'));
+    res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 
